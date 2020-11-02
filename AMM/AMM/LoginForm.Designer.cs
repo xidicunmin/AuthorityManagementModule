d1a0117a55700f7b719a53b52a4987b665ffa214
xidicunmin <redwangshifu@sina.com>
2020-11-02 11:20:21 +0800

    gongnengliuchengtu

--- AMM/AMM/LoginForm.Designer.cs
+++ AMM/AMM/LoginForm.Designer.cs
@@ -29,24 +29,190 @@
         private void InitializeComponent()
         {
             this.pnlLogin = new System.Windows.Forms.Panel();
+            this.txtPwdT = new System.Windows.Forms.TextBox();
+            this.lblPwdT = new System.Windows.Forms.Label();
+            this.cmbLevel = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.txtPwd = new System.Windows.Forms.TextBox();
+            this.lblPwd = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.dgvUserList = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.pnlLogin.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUserList)).BeginInit();
             this.SuspendLayout();
             // 
             // pnlLogin
             // 
+            this.pnlLogin.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
             this.pnlLogin.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlLogin.Controls.Add(this.txtPwdT);
+            this.pnlLogin.Controls.Add(this.lblPwdT);
+            this.pnlLogin.Controls.Add(this.cmbLevel);
+            this.pnlLogin.Controls.Add(this.label1);
+            this.pnlLogin.Controls.Add(this.btnLogin);
+            this.pnlLogin.Controls.Add(this.txtPwd);
+            this.pnlLogin.Controls.Add(this.lblPwd);
+            this.pnlLogin.Controls.Add(this.txtName);
+            this.pnlLogin.Controls.Add(this.lblName);
             this.pnlLogin.Location = new System.Drawing.Point(6, 6);
             this.pnlLogin.Name = "pnlLogin";
-            this.pnlLogin.Size = new System.Drawing.Size(372, 249);
+            this.pnlLogin.Size = new System.Drawing.Size(372, 545);
             this.pnlLogin.TabIndex = 0;
+            // 
+            // txtPwdT
+            // 
+            this.txtPwdT.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtPwdT.Location = new System.Drawing.Point(152, 184);
+            this.txtPwdT.Margin = new System.Windows.Forms.Padding(16);
+            this.txtPwdT.Name = "txtPwdT";
+            this.txtPwdT.Size = new System.Drawing.Size(181, 35);
+            this.txtPwdT.TabIndex = 8;
+            this.txtPwdT.UseSystemPasswordChar = true;
+            // 
+            // lblPwdT
+            // 
+            this.lblPwdT.AutoSize = true;
+            this.lblPwdT.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblPwdT.Location = new System.Drawing.Point(16, 187);
+            this.lblPwdT.Name = "lblPwdT";
+            this.lblPwdT.Size = new System.Drawing.Size(130, 24);
+            this.lblPwdT.TabIndex = 7;
+            this.lblPwdT.Text = "确认密码：";
+            // 
+            // cmbLevel
+            // 
+            this.cmbLevel.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.cmbLevel.FormattingEnabled = true;
+            this.cmbLevel.Items.AddRange(new object[] {
+            "管理员",
+            "操作员",
+            "ME",
+            "机长"});
+            this.cmbLevel.Location = new System.Drawing.Point(152, 251);
+            this.cmbLevel.Margin = new System.Windows.Forms.Padding(16);
+            this.cmbLevel.Name = "cmbLevel";
+            this.cmbLevel.Size = new System.Drawing.Size(181, 32);
+            this.cmbLevel.TabIndex = 6;
+            this.cmbLevel.Text = "管理员";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(64, 254);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 24);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "等级：";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnLogin.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnLogin.Location = new System.Drawing.Point(287, 500);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(80, 40);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "登录";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // txtPwd
+            // 
+            this.txtPwd.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtPwd.Location = new System.Drawing.Point(152, 117);
+            this.txtPwd.Margin = new System.Windows.Forms.Padding(16);
+            this.txtPwd.Name = "txtPwd";
+            this.txtPwd.Size = new System.Drawing.Size(181, 35);
+            this.txtPwd.TabIndex = 3;
+            this.txtPwd.UseSystemPasswordChar = true;
+            // 
+            // lblPwd
+            // 
+            this.lblPwd.AutoSize = true;
+            this.lblPwd.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblPwd.Location = new System.Drawing.Point(64, 120);
+            this.lblPwd.Name = "lblPwd";
+            this.lblPwd.Size = new System.Drawing.Size(82, 24);
+            this.lblPwd.TabIndex = 2;
+            this.lblPwd.Text = "密码：";
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtName.Location = new System.Drawing.Point(152, 50);
+            this.txtName.Margin = new System.Windows.Forms.Padding(16);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(181, 35);
+            this.txtName.TabIndex = 1;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblName.Location = new System.Drawing.Point(40, 53);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(106, 24);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "用户名：";
             // 
             // panel1
             // 
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.dgvUserList);
             this.panel1.Location = new System.Drawing.Point(384, 6);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(394, 549);
             this.panel1.TabIndex = 1;
+            // 
+            // dgvUserList
+            // 
+            this.dgvUserList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvUserList.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dgvUserList.Location = new System.Drawing.Point(3, 3);
+            this.dgvUserList.Name = "dgvUserList";
+            this.dgvUserList.RowTemplate.Height = 23;
+            this.dgvUserList.Size = new System.Drawing.Size(386, 541);
+            this.dgvUserList.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "用户名";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "密码";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "等级";
+            this.Column3.Items.AddRange(new object[] {
+            "管理员",
+            "操作员",
+            "ME",
+            "机长"});
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 90;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "操作";
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 50;
             // 
             // LoginForm
             // 
@@ -55,12 +221,18 @@
             this.ClientSize = new System.Drawing.Size(784, 561);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.pnlLogin);
+            this.MaximizeBox = false;
             this.MaximumSize = new System.Drawing.Size(800, 600);
+            this.MinimizeBox = false;
             this.MinimumSize = new System.Drawing.Size(400, 300);
             this.Name = "LoginForm";
             this.Padding = new System.Windows.Forms.Padding(3);
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "登录";
+            this.pnlLogin.ResumeLayout(false);
+            this.pnlLogin.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUserList)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -69,5 +241,19 @@
 
         private System.Windows.Forms.Panel pnlLogin;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox cmbLevel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox txtPwd;
+        private System.Windows.Forms.Label lblPwd;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.DataGridView dgvUserList;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewComboBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewButtonColumn Column4;
+        private System.Windows.Forms.TextBox txtPwdT;
+        private System.Windows.Forms.Label lblPwdT;
     }
 }
